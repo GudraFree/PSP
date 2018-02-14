9e6853ff904891bdbc1528cae7c102b38bf5f306
GudraFree <perig1607@hotmail.com>
2018-02-14 14:26:35 +0100

    Odio mi vida

---
+++ src/tema3/ejercicio03/AhorcadoClient.java
@@ -0,0 +1,110 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tema3.ejercicio03;
+
+import tema3.ejercicio02.*;
+import tema3.ejercicio01.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Perig
+ */
+public class AhorcadoClient {
+    private static final String[] AHORCADO = { 
+"------|\n" +
+"|     \n" +
+"|    \n" +
+"|    \n" +
+"|\n" +
+"====", 
+"------|\n" +
+"|     O\n" +
+"|    \n" +
+"|    \n" +
+"|\n" +
+"====",
+"------|\n" +
+"|     O\n" +
+"|     |\n" +
+"|    \n" +
+"|\n" +
+"====",
+"------|\n" +
+"|     O\n" +
+"|    /|\n" +
+"|    \n" +
+"|\n" +
+"====",
+"------|\n" +
+"|     O\n" +
+"|    /|\\\n" +
+"|    \n" +
+"|\n" +
+"====",
+"------|\n" +
+"|     O\n" +
+"|    /|\\\n" +
+"|    / \n" +
+"|\n" +
+"====",
+"------|\n" +
+"|     O\n" +
+"|    /|\\\n" +
+"|    / \\\n" +
+"|\n" +
+"====", };
+    
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        String host = "localhost";
+        int port = 4444;
+        Socket socket = null;
+        PrintWriter out = null;
+        BufferedReader in = null;
+        try {
+            socket = new Socket(host, port);
+            out = new PrintWriter(socket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        } catch (UnknownHostException e) {
+            System.err.println("Error, host no encontrado");
+            System.exit(1);
+        } catch (IOException e) {
+            System.err.println("Error de E/S");
+            System.exit(1);
+        }
+        
+        System.out.println("¡Bienvenido al juego del Ahorcado!");
+        String l="";
+        while((l=in.readLine())!=null) {
+//            System.out.println("Client: iteración");
+            String[] lectura = l.split("-");
+            String mensaje = lectura[0];
+            String palabra = lectura[1];
+            int errores = Integer.parseInt(lectura[2]);
+            System.out.println(AHORCADO[errores]);
+            System.out.println("\n    "+palabra+"\n");
+            System.out.println(mensaje);
+            if(!mensaje.equals("Hasta otra")) {
+                String letra = sc.nextLine();
+                out.println(letra);
+//                System.out.println("Cliente: enviada "+letra);
+            } else break;
+        }
+        
+        in.close();
+        out.close();
+        
+        socket.close();
+    }
+    
+}
---
+++ src/tema3/ejercicio03/Utils.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tema3.ejercicio03;
+
+/**
+ *
+ * @author Perig
+ */
+public class Utils {
+    // estados
+    public final int LOGIN_OPTIONS = 0;
+    public final int L_NAME = 1;
+    public final int L_PASS = 2;
+    public final int R_NAME = 3;
+    public final int R_PASS = 4;
+    public final int R_ADMIN = 5;
+    public final int OPTIONS = 6;
+    public final int QUERY_WHO = 7;
+    public final int ASKED4LETTER = 8;
+    public final int ANOTHER = 9;
+}
